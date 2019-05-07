29e858224b88024204c7e4874cf4af91713773cc
Sddddddddddddddddddrr <49367343+Sddddddddddddddddddrr@users.noreply.github.com>
2019-05-07 18:12:39 +0500

    Update AppTest7.java

--- src/test/java/pi/KR/AppTest7.java
+++ src/test/java/pi/KR/AppTest7.java
@@ -3,18 +3,9 @@ package pi.KR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-/**
- * В теле данного класса расположен метод для проверки точности
- * расчётов суммы вклада
- */
 public class AppTest7 {
 	
-	CalcMain api = new CalcMain();
-	
-	@Test
-	public void testRunApp() {
-		new CalcMain(); //can it run application
-	}
+CalcMain api = new CalcMain();
 
     @Test
     public void testApp(){
