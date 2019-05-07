2c708e9b7658b9cb7470f0ff739f6b5a801ddf92
Sddddddddddddddddddrr <49367343+Sddddddddddddddddddrr@users.noreply.github.com>
2019-05-07 18:12:18 +0500

    Update AppTest6.java

--- src/test/java/pi/KR/AppTest6.java
+++ src/test/java/pi/KR/AppTest6.java
@@ -2,19 +2,10 @@ package pi.KR;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-/**
- * В теле данного класса расположен метод для проверки точности
- * расчётов суммы процентов вклада с учётом капитализации
- */
+
 public class AppTest6 {
 	
-	
 CalcMain api = new CalcMain();
-	
-	@Test
-	public void testRunApp() {
-		new CalcMain(); //can it run application
-	}
 
     @Test
     public void testApp(){
